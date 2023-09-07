2c7c60031feb980b80308bf5caccf213a484eace
rintheo <107115767+rintheo@users.noreply.github.com>
2023-09-08 00:27:44 +0800

    Reorder functions in controllerDOM script

--- src/scripts/controllerDOM.js
+++ src/scripts/controllerDOM.js
@@ -40,8 +40,8 @@ const switchPlayers = () => {
   currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
   targetPlayer = targetPlayer === players[0] ? players[1] : players[0];
 
-  console.log(`Current Player: ${currentPlayer.name}`);
-  console.log(`Target Player: ${targetPlayer.name}`);
+  // console.log(`Current Player: ${currentPlayer.name}`);
+  // console.log(`Target Player: ${targetPlayer.name}`);
 };
 
 const checkWinningCondition = () => {
@@ -50,70 +50,6 @@ const checkWinningCondition = () => {
     return true;
   }
   return false;
-};
-
-const updateHP = () => {
-  const player = targetPlayer === players[0]
-    ? 'current'
-    : 'enemy';
-  const totalHP = targetPlayer
-    .board
-    .fleet
-    .reduce((total, ship) => total + ship.length, 0);
-  const currentHP = targetPlayer
-    .board
-    .fleet
-    .reduce((total, ship) => total - ship.hits, totalHP);
-  const health = `${Math.round((currentHP / totalHP) * 100)}%`;
-  const hit = `${Math.round((1 / 17) * 100)}%`;
-  const empty = `${Math.round((1 - currentHP / totalHP) * 100)}%`;
-
-  const playerHealthText = document.querySelector(`.${player} .health > .text `);
-  playerHealthText.textContent = `${currentHP}/${totalHP}`;
-
-  document.documentElement.style.setProperty(`--${player}-health`, health);
-  document.documentElement.style.setProperty(`--${player}-hit`, hit);
-
-  const playerHealthBar = document.querySelector(`.${player} .health > .bar `);
-  playerHealthBar.addEventListener('transitionend', () => {
-    document.documentElement.style.setProperty(`--${player}-hit`, '0%');
-    document.documentElement.style.setProperty(`--${player}-empty`, empty);
-  }, { once: true });
-};
-
-const clearBoard = () => {
-  const boards = document.querySelector('.boards');
-  while (boards.firstChild) {
-    boards.removeChild(boards.firstChild);
-  }
-};
-
-const initializeBoard = () => {
-  players.forEach((player) => {
-    const boards = document.querySelector('.boards');
-    const board = document.createElement('div');
-    board.classList.add('board');
-    if (player === currentPlayer) board.classList.add('current');
-    if (player === targetPlayer) board.classList.add('target');
-
-    for (let i = 9; i >= 0; i -= 1) {
-      for (let j = 0; j < 10; j += 1) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.dataset.x = j;
-        cell.dataset.y = i;
-        cell.dataset.playerName = player.name;
-        if (
-          player === targetPlayer
-          && currentPlayer instanceof Player
-          && !(currentPlayer instanceof AI)
-        ) cell.addEventListener('click', hitCellPlayer);
-        board.appendChild(cell);
-      }
-    }
-    boards.appendChild(board);
-  });
-  printSprites();
 };
 
 const hitExplosionEffect = async (targetCell) => {
@@ -159,6 +95,35 @@ const addMissMark = (targetCell) => {
   mark.outerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>miss</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>';
 };
 
+const updateHP = () => {
+  const player = targetPlayer === players[0]
+    ? 'current'
+    : 'enemy';
+  const totalHP = targetPlayer
+    .board
+    .fleet
+    .reduce((total, ship) => total + ship.length, 0);
+  const currentHP = targetPlayer
+    .board
+    .fleet
+    .reduce((total, ship) => total - ship.hits, totalHP);
+  const health = `${Math.round((currentHP / totalHP) * 100)}%`;
+  const hit = `${Math.round((1 / 17) * 100)}%`;
+  const empty = `${Math.round((1 - currentHP / totalHP) * 100)}%`;
+
+  const playerHealthText = document.querySelector(`.${player} .health > .text `);
+  playerHealthText.textContent = `${currentHP}/${totalHP}`;
+
+  document.documentElement.style.setProperty(`--${player}-health`, health);
+  document.documentElement.style.setProperty(`--${player}-hit`, hit);
+
+  const playerHealthBar = document.querySelector(`.${player} .health > .bar `);
+  playerHealthBar.addEventListener('transitionend', () => {
+    document.documentElement.style.setProperty(`--${player}-hit`, '0%');
+    document.documentElement.style.setProperty(`--${player}-empty`, empty);
+  }, { once: true });
+};
+
 const updateBoard = async ([x, y]) => {
   const targetCell = document.querySelector(`.cell[data-x="${x}"][data-y="${y}"][data-player-name="${targetPlayer.name}"]`);
   await hitExplosionEffect(targetCell);
@@ -169,28 +134,6 @@ const updateBoard = async ([x, y]) => {
     targetCell.classList.add('miss');
     addMissMark(targetCell);
   }
-};
-
-const printSprites = () => {
-  currentPlayer.board.getFleet().forEach((ship) => {
-    const { x, y } = ship.bowCoordinates;
-    const bowCell = document.querySelector(`.cell[data-x="${x}"][data-y="${y}"]`);
-    bowCell.classList.add('ship-bow');
-
-    const spriteContainer = document.createElement('div');
-    spriteContainer.classList.add('sprite-container');
-    if (ship.isHorizontal) spriteContainer.classList.add('horizontal');
-
-    const shipSprite = document.createElement('img');
-    shipSprite.src = shipSprites.find((sprite) => sprite.type === ship.type).src;
-    shipSprite.classList.add('sprite');
-
-    const spriteScale = 0.35 * (parseInt(window.getComputedStyle(bowCell).width, 10) / 60);
-    document.documentElement.style.setProperty('--sprite-scale', spriteScale);
-
-    spriteContainer.appendChild(shipSprite);
-    bowCell.appendChild(spriteContainer);
-  });
 };
 
 const showInputBlocker = () => {
@@ -239,15 +182,72 @@ const hitCellPlayer = (e) => {
   }
 };
 
+const printSprites = () => {
+  currentPlayer.board.getFleet().forEach((ship) => {
+    const { x, y } = ship.bowCoordinates;
+    const bowCell = document.querySelector(`.cell[data-x="${x}"][data-y="${y}"]`);
+    bowCell.classList.add('ship-bow');
+
+    const spriteContainer = document.createElement('div');
+    spriteContainer.classList.add('sprite-container');
+    if (ship.isHorizontal) spriteContainer.classList.add('horizontal');
+
+    const shipSprite = document.createElement('img');
+    shipSprite.src = shipSprites.find((sprite) => sprite.type === ship.type).src;
+    shipSprite.classList.add('sprite');
+
+    const spriteScale = 0.35 * (parseInt(window.getComputedStyle(bowCell).width, 10) / 60);
+    document.documentElement.style.setProperty('--sprite-scale', spriteScale);
+
+    spriteContainer.appendChild(shipSprite);
+    bowCell.appendChild(spriteContainer);
+  });
+};
+
 const resizeSprites = () => {
   const bowCell = document.querySelector('.cell');
   const spriteScale = 0.35 * (parseInt(window.getComputedStyle(bowCell).width, 10) / 60);
   document.documentElement.style.setProperty('--sprite-scale', spriteScale);
 };
 
+const clearBoard = () => {
+  const boards = document.querySelector('.boards');
+  while (boards.firstChild) {
+    boards.removeChild(boards.firstChild);
+  }
+};
+
+const initializeBoard = () => {
+  players.forEach((player) => {
+    const boards = document.querySelector('.boards');
+    const board = document.createElement('div');
+    board.classList.add('board');
+    if (player === currentPlayer) board.classList.add('current');
+    if (player === targetPlayer) board.classList.add('target');
+
+    for (let i = 9; i >= 0; i -= 1) {
+      for (let j = 0; j < 10; j += 1) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.dataset.x = j;
+        cell.dataset.y = i;
+        cell.dataset.playerName = player.name;
+        if (
+          player === targetPlayer
+          && currentPlayer instanceof Player
+          && !(currentPlayer instanceof AI)
+        ) cell.addEventListener('click', hitCellPlayer);
+        board.appendChild(cell);
+      }
+    }
+    boards.appendChild(board);
+  });
+  printSprites();
+};
+
 window.addEventListener('resize', resizeSprites);
 
 initializeBoard();
 
-console.log(`Current Player: ${currentPlayer.name}`);
-console.log(`Target Player: ${targetPlayer.name}`);
+// console.log(`Current Player: ${currentPlayer.name}`);
+// console.log(`Target Player: ${targetPlayer.name}`);
