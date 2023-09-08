43146e91b02b88b49bf86564189e5d0a82c45a92
rintheo <107115767+rintheo@users.noreply.github.com>
2023-09-08 23:05:57 +0800

    Add method to GameBoard class
    - Added method that returns an array of cell coordinates containing
    ships

--- src/scripts/gameBoard.js
+++ src/scripts/gameBoard.js
@@ -67,4 +67,18 @@ export default class GameBoard {
     }
     return unhitCoordinates;
   }
+
+  allShipCoordinates() {
+    const shipCoordinates = [];
+    for (let i = 0; i < 10; i += 1) {
+      for (let j = 0; j < 10; j += 1) {
+        if (this.board[i][j].ship !== null) {
+          if (!this.board[i][j].isHit) {
+            shipCoordinates.push([i, j]);
+          }
+        }
+      }
+    }
+    return shipCoordinates;
+  }
 }
