2ff67140f28682486e3454712d0666037fe88d62
rintheo <107115767+rintheo@users.noreply.github.com>
2023-09-15 03:11:09 +0800

    Remove unused commented code from game.js

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -5,39 +5,6 @@ import Ship from './ship';
 
 export const players = [];
 
-// const ships = [
-//   {
-//     type: 'Carrier',
-//     length: 5,
-//     tempCoords: [3, 2],
-//     tempHori: true,
-//   },
-//   {
-//     type: 'Battleship',
-//     length: 4,
-//     tempCoords: [3, 5],
-//     tempHori: true,
-//   },
-//   {
-//     type: 'Cruiser',
-//     length: 3,
-//     tempCoords: [8, 3],
-//     tempHori: false,
-//   },
-//   {
-//     type: 'Submarine',
-//     length: 3,
-//     tempCoords: [6, 7],
-//     tempHori: false,
-//   },
-//   {
-//     type: 'Destroyer',
-//     length: 2,
-//     tempCoords: [1, 9],
-//     tempHori: true,
-//   },
-// ];
-
 export const generatePlayers = (inputPlayers) => {
   players.length = 0;
 
