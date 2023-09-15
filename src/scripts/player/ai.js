f212c012592cfa0196ad1b544c7794cecd59f736
rintheo <107115767+rintheo@users.noreply.github.com>
2023-09-15 23:04:28 +0800

    Add AI Normal logic

--- src/scripts/player/ai.js
+++ src/scripts/player/ai.js
@@ -4,6 +4,13 @@ export default class AI extends Player {
   constructor(difficulty = 'Easy') {
     super(`AI ${difficulty}`);
     this.difficulty = difficulty;
+    this.memory = {
+      lastHitCoordinate: null,
+      lastHitSuccess: false,
+      lastHitSunkAShip: false,
+      successfulHits: [],
+      toTryCoordinates: [],
+    };
   }
 
   chooseAttackCoordinates(player) {
@@ -13,12 +20,71 @@ export default class AI extends Player {
         Math.floor(Math.random() * availableAttackCoordinates.length)
       ];
     }
-    // Normal Placeholder, same as easy
     if (this.difficulty === 'Normal') {
       const availableAttackCoordinates = player.board.allUnhitCoordinates();
-      return availableAttackCoordinates[
-        Math.floor(Math.random() * availableAttackCoordinates.length)
-      ];
+      let x;
+      let y;
+      if (this.memory.lastHitSuccess) {
+        [x, y] = this.memory.lastHitCoordinate;
+        this.memory.lastHitSunkAShip = player.board.getBoard()[x][y].ship.hasSunk;
+        if (this.memory.lastHitSunkAShip) {
+          this.memory.toTryCoordinates.length = 0;
+        } else {
+          const filtered = this.memory.successfulHits
+            .filter((item) => (
+              ((item[0] === x - 1) && (item[1] === y))
+                    || ((item[0] === x + 1) && (item[1] === y))
+                    || ((item[0] === x) && (item[1] === y - 1))
+                    || ((item[0] === x) && (item[1] === y + 1))
+            ));
+          this.memory.toTryCoordinates.length = 0;
+          // Check for line and list possible attack coordinates
+          filtered.forEach((coordinate) => {
+            if (coordinate[0] === x) {
+              this.memory.toTryCoordinates.push(...availableAttackCoordinates
+                .filter((item) => (
+                  ((item[0] === coordinate[0]) && (item[1] === coordinate[1] - 1))
+                        || ((item[0] === coordinate[0]) && (item[1] === coordinate[1] - 2))
+                        || ((item[0] === coordinate[0]) && (item[1] === coordinate[1] + 1))
+                        || ((item[0] === coordinate[0]) && (item[1] === coordinate[1] + 2))
+                )));
+            } else if (coordinate[1] === y) {
+              this.memory.toTryCoordinates.push(...availableAttackCoordinates
+                .filter((item) => (
+                  ((item[0] === coordinate[0] - 1) && (item[1] === coordinate[1]))
+                        || ((item[0] === coordinate[0] - 2) && (item[1] === coordinate[1]))
+                        || ((item[0] === coordinate[0] + 1) && (item[1] === coordinate[1]))
+                        || ((item[0] === coordinate[0] + 2) && (item[1] === coordinate[1]))
+                )));
+            }
+          });
+          // If previous check did not see any lines,
+          // Just check surrounding 4 cells
+          if (this.memory.toTryCoordinates.length === 0) {
+            this.memory.toTryCoordinates.push(...availableAttackCoordinates
+              .filter((item) => (
+                ((item[0] === x - 1) && (item[1] === y))
+                      || ((item[0] === x + 1) && (item[1] === y))
+                      || ((item[0] === x) && (item[1] === y - 1))
+                      || ((item[0] === x) && (item[1] === y + 1))
+              )));
+          }
+        }
+      }
+      if (this.memory.toTryCoordinates.length > 0) {
+        [x, y] = this.memory.toTryCoordinates.pop();
+      } else {
+        [x, y] = availableAttackCoordinates[
+          Math.floor(Math.random() * availableAttackCoordinates.length)
+        ];
+      }
+      this.memory.lastHitCoordinate = [x, y];
+      this.memory.lastHitSuccess = Boolean(player.board.getBoard()[x][y].ship);
+      this.memory.lastHitSunkAShip = false;
+      if (this.memory.lastHitSuccess) {
+        this.memory.successfulHits.push(this.memory.lastHitCoordinate);
+      }
+      return [x, y];
     }
     if (this.difficulty === 'Cheat') {
       const availableAttackCoordinates = player.board.allShipCoordinates();
